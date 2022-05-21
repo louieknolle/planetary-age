f00eaf330641fdba863f29547e604104b045bbf4
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-05-20 20:16:49 -0400

    write test for age constructor

---
+++ __tests__/age.test.js
@@ -0,0 +1,13 @@
+import Age from '../src/js/age.js';
+
+describe('Age', () => {
+  let age;
+  beforeEach(() => {
+    age = new Age(30, 76);
+  });
+
+  test('should correctly create an Age object', () => {
+    expect(age.earthAge).toEqual(30);
+    expect(age.lifeExpectancy).toEqual(76);
+  });
+});
