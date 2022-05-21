fbfa59c2008a72dfc5a53bc207bdfde6a4016531
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-05-20 20:21:16 -0400

    write age constructor and pass test for it

---
+++ src/js/age.js
@@ -0,0 +1,10 @@
+export default class Age {
+  constructor(age, lifeExpect) {
+    this.earthAge = age;
+    this.lifeExpectancy= lifeExpect;
+  }
+
+  // example () {
+  //   //Example method for template
+  // }
+}
